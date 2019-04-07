80a4dab52594e47249a18a5a6c005ec760e0f4ca
fl0wart <47152380+fl0wart@users.noreply.github.com>
2019-04-07 11:09:21 +0300

    Create eval.js

---
+++ Commands/eval.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const util = require('util');
+const TOKEN = process.env.TOKEN;
+
+exports.run = async (bot, message , args) => {
+if (message.author.id != '487534691663085569') return message.channel.send('❌ This command is only for **Developers**.');
+let code = args.join(' ');            
+try {
+        let ev = eval(code)
+                        let str = util.inspect(ev, {
+                            depth: 1
+                        })
+                        str = `${str.replace(new RegExp(`${TOKEN}|${process.env.TOKEN}`, "g"), "yungdrew was here")}`;
+                        if(str.length > 1800) {
+                            str = str.substr(0, 1800)
+                            str = str + "..."
+                        }
+            message.channel.send("", { embed: { 
+            color: 2551400,      
+        fields: [{        
+            name: '**:inbox_tray: Input**',     
+            value: '\`\`\`' + code + '\`\`\`'         
+        },{     
+            name: '**:outbox_tray: Output**', 
+                value: '\`\`\`' + str + '\`\`\`'  
+                }], 
+            footer: {     
+            text: ``    }     
+        }});
+    } catch (err) {   
+
+        message.channel.send("", { embed: { 
+            color: 2551400,      
+        fields: [{        
+            name: '**:inbox_tray: Input**',     
+            value: '\`\`\`' + code + '\`\`\`'         
+        },{     
+            name: '**:outbox_tray: Output**', 
+                value: '\`\`\`' + err + '\`\`\`'  
+                }], 
+            footer: {     
+            text: ``    }     }});    } 
+        }; 
