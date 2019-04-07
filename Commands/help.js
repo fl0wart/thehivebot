7dd23d177cf73f53185c65744115a40f4b0e89f2
fl0wart <47152380+fl0wart@users.noreply.github.com>
2019-04-07 11:16:01 +0300

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {  
 
   message.channel.send(new Discord.RichEmbed()
-        .setDescription(`[🔷] **Available Commands!**\n\n\Moderation Commands! (🔩)\n\n\`ban\` , \`kick\` , \`purge\`, \`addrole\`, \`removerole\`, \`mute\`  \n\nDefault Commands! (💻)\n\n\`roleinfo\`, \`8ball\`,  \`avatar\` , \`help\`\n\n\Developer Commands! (⭐)\n\n\`eval\`, \`reboot\, \`say\```)
+        .setDescription(`[🔷] **Available Commands!**\n\n\Moderation Commands! (🔩)\n\n\`ban\` , \`kick\` , \`purge\`, \`addrole\`, \`removerole\`, \`mute\`  \n\nDefault Commands! (💻)\n\n\`roleinfo\`, \`8ball\`,  \`avatar\` , \`serverinfo\`\n\n\Developer Commands! (⭐)\n\n\`eval\`, \`reboot\, \`say\```)
         .setColor('#25c059')
     );
 }
